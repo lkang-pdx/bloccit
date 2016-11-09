8dd887df5d2cb8d1c10f7255cc71477f8e6ddd50
Luna Kang <luna.kang.ca@gmail.com>
2016-11-09 06:21:52 -0800

    A31 Favorites completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -13,4 +13,16 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, subject: "New comment on #{post.title}")
   end
+
+  def new_post(post)
+
+    headers["Message-ID"] = "<posts/#{post.id}@afternoon-crag-43986.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@afternoon-crag-43986.example>"
+    headers["References"] = "<post/#{post.id}@afternoon-crag-43986.example>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "You're following, #{post.title}")
+  end
+
 end
